d69272c757d9f90f3ccb55f4e45c4a0b06f8669b
swadhkitchenindia-cloud <swadh.kitchen.india@gmail.com>
2026-05-21 10:37:07 +0530

    V8

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,9 +4,9 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAE7XX-8Jn3T1tgZBBzuSu22OJ1HtNLiGE",
-  authDomain: "saverrnapp.firebaseapp.com",
-  projectId: "saverrnapp",
-  storageBucket: "saverrnapp.firebasestorage.app",
+  authDomain: "foodsavernapp.firebaseapp.com",
+  projectId: "foodsavernapp",
+  storageBucket: "foodsavernapp.firebasestorage.app",
   messagingSenderId: "610582580020",
   appId: "1:610582580020:web:e83124f479b3d38f71b361",
   measurementId: "G-JMSDKRJ5ZJ"
